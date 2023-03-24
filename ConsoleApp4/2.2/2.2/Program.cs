c1ad9be7e295b0f6996431e56a97ede29be93c9d
FlorenzeN123 <127619202+FlorenzeN123@users.noreply.github.com>
2023-03-24 15:05:59 +0300

    PR1

---
+++ ConsoleApp4/2.2/2.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2
+{
+
+    internal class Program
+    {
+        class TaskSolution
+        {
+            private int n = 0;
+            public bool setNumber(int num)
+            {
+                if (n + 1 == num)
+                {
+                    this.n = num;
+                    return true;
+                }
+                else
+                {
+                    this.n = 0;
+                    return false;
+                }
+            }
+            public void returnExpectedResult()
+            {
+                Console.WriteLine(n + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            TaskSolution shved = new TaskSolution();
+            int n = 0;
+            while (true)
+            {
+                Console.Write("Введите число:");
+                shved.returnExpectedResult();
+                Console.WriteLine(shved.setNumber(int.Parse(Console.ReadLine())));
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/2.3/2.3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    class CaesarCode
+    {
+        private int move = 1;
+
+
+        public CaesarCode(int move)
+        {
+            this.move = move;
+        }
+
+        //Шифровка
+        public string Encrypt(string text)
+        {
+            string caesarText = "";
+            foreach (char abc in text)
+            {
+                if (char.IsLetter(abc))
+                {
+                    char movingAbc = (char)(((int)char.ToUpper(abc) + move - 1040) % 32 + 1040);
+                    movingAbc += movingAbc;
+                }
+                else
+                {
+                    caesarText += abc;
+                }
+            }
+            return caesarText;
+        }
+
+        // Расшифровка
+        public string Decrypt(string caesarText)
+        {
+            string text = "";
+            foreach (char abc in caesarText)
+            {
+                if (char.IsLetter(abc))
+                {
+                    char movingAbc = (char)(((int)char.ToUpper(abc) - move - 1040 + 32) % 32 + 1040);
+                }
+                else
+                {
+                    text += abc;
+                }
+            }
+            return text;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            CaesarCode Caesar = new CaesarCode(1);
+
+            string text = "Швецов Артём Староста";
+            string caesarText = "Швецов Артём Староста";
+
+            Console.WriteLine($"Зашифрованный текст: {Caesar.Encrypt(text)}");
+            Console.WriteLine($"Расшифрованный текст: {Caesar.Decrypt(caesarText)}");
+        }
+    }
+}
